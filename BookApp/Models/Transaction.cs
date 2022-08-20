1d2f5f4cb599c60bbe001376d865badfb6376bcb
AVITA <AVITA@DESKTOP-MAL7OGK>
2022-08-20 16:21:46 +0530

    Add project files.

---
+++ BookApp/Models/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookApp.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Country { get; set; }
+  
+        public string First_Name { get; set; }
+     
+        public string Last_Name { get; set; }
+  
+        public string Address { get; set; }
+    
+        public string Apartment { get; set; }
+     
+
+        public string city { get; set; }
+      
+        public string state { get; set; }
+ 
+        public string Pin_Code { get; set; }
+
+
+        public string Phone_Number { get; set; }
+     
+        public int User_Id { get; set; }
+    }
+}
---
+++ BookApp/Models/UserAccounts.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookApp.Models
+{
+    public class UserAccounts
+    {
+        [Key]
+       
+        
+        public string UserId { get; set; }
+      
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+
+     
+        
+        public string EmailAddress { get; set; }
+       
+        public string password { get; set; }
+     
+        public string ConfirmPassword { get; set; }
+       
+        public string Role { get; set; }
+    }
+}
